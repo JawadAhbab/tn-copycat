edd32840419eaaac38ea40d522b305b65e733dcc
JawadAhbab <jawad.ahbab.turna@gmail.com>
2023-07-29 00:25:42 +0600

    🔥 The copyfiles will be readonly

--- src/accessories/execWatch.ts
+++ src/accessories/execWatch.ts
@@ -1,4 +1,4 @@
-import { chmodSync, copyFileSync, ensureDirSync } from 'fs-extra'
+import { chmodSync, copyFileSync, ensureDirSync, existsSync } from 'fs-extra'
 import rimraf from 'rimraf'
 type Event = 'add' | 'addDir' | 'change' | 'unlink' | 'unlinkDir'
 
@@ -7,7 +7,7 @@ export const execWatch = (event: Event, frompath: string, topath: string, readon
     if (event === 'addDir') return ensureDirSync(topath)
     if (event === 'unlink' || event === 'unlinkDir') return rimraf(topath, () => null)
     if (event === 'add' || event === 'change') {
-      chmodSync(topath, 0o666)
+      if (existsSync(topath)) chmodSync(topath, 0o666)
       copyFileSync(frompath, topath)
       chmodSync(topath, readonly ? 0o444 : 0o666)
     }
